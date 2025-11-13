10b489b127a05b75461f11e3315e10e73fae63e5
Wendel <jwend3l@gmail.com>
2025-11-13 16:23:11 -0300

    feat: User implementado com requisições POST e GET funcionando.

---
+++ javalin/src/main/java/javalin/ufpb/wendel/models/user/User.java
@@ -0,0 +1,16 @@
+package javalin.ufpb.wendel.models.user;
+import lombok.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String nome;
+
+    private String email;
+
+    private int idade;
+
+}
